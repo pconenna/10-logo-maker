19c7e265608c11f7cd7964c5e48b84bd827a2c4a
Peter Conenna <pconenna@depaul.edu>
2023-06-07 15:14:02 -0500

    started prompt in index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const inquirer = require("inquirer");
+
+function init(){
+    inquirer.prompt([
+        {
+            type: 'input', 
+            message: 'Enter logo text (3 character maximum)',
+            name: 'text'
+        },
+        {
+            type: 'input', 
+            message: 'Enter text color',
+            name: 'textColor'
+        },
+        {
+            type: 'checkbox', 
+            message: 'Choose a shape',
+            choices: ['circle','square','triangle'],
+            name: 'shape'
+        },
+        {
+            type: 'input', 
+            message: 'Enter shape color',
+            name: 'shapeColor'
+        }
+    ]).then((response)=>{
+        //call shape constructor with response data
+        // placeholder code for now
+        console.log(`Your logo will be a ${response.shapeColor} ${response.shape} that says ${response.text} in ${response.textColor} font.`)
+    })
+}
+
+init();
